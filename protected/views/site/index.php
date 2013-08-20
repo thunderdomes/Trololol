6910979c83fe7474a8e0e0869970213ffe09dc22
lateph <lateph@gmail.com>
2013-08-20 11:41:27 +0700

    init

---
+++ protected/views/site/index.php
@@ -0,0 +1,16 @@
+<div id="posts">
+<?php foreach($posts as $post): ?>
+    <div class="post">
+        <p>Autor: <?php echo $post->author->username; ?></p>
+        <p><?php echo $post->content; ?></p>
+    </div>
+	<hr>
+<?php endforeach; ?>
+</div>
+<?php $this->widget('ext.yiinfinite-scroll.YiinfiniteScroller', array(
+    'contentSelector' => '#posts',
+    'itemSelector' => 'div.post',
+    'loadingText' => 'Loading...',
+    'donetext' => 'This is the end... my only friend, the end',
+    'pages' => $pages,
+)); ?>
