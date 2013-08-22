ccde7d7365de6ebfbddb4ff0eaa77bdf7b35b7fa
Arie <arie@wego.com>
2013-08-23 00:00:37 +0700

    update new css

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,22 +1,8 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
-	<div id="content">
-		<?php echo $content; ?>
-	</div><!-- content -->
-</div>
-<div class="span-5 last">
-	<div id="sidebar">
-	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations'),
-		));
-		$this->endWidget();
-	?>
+	<div class="sidebar">
+		ini sidebar
 	</div><!-- sidebar -->
-</div>
+		<?php echo $content; ?>
+
 <?php $this->endContent(); ?>
