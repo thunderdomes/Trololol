1225bdc74cbce8ea809bd87b2a4f1a24f7780c52
lateph <lateph@gmail.com>
2013-08-31 16:53:39 +0700

    commit

--- protected/config/main.php
+++ protected/config/main.php
@@ -45,6 +45,7 @@ return array(
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
+				'image/default/create/'=>'image/default/create',
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
